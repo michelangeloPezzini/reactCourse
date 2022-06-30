7cc933916eebc4f8740c24e70f88fe798fe147f0
michelangelo.pezzini@compasso.com.br <michelangelo.pezzini@compasso.com.br>
2022-06-30 11:08:55 -0300

    Update App.js

--- react_youtube_course/src/App.js
+++ react_youtube_course/src/App.js
@@ -9,7 +9,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <p>teste para commit</p>
+        <p>teste para comitar mudanças</p>
         <p>Teste</p>
         <a
           className="App-link"
